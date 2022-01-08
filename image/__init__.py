a19cf92332cb83b8e6d81b833ce9a2d96a273d26
justfoolingaround <kr.justfoolingaround@gmail.com>
2022-01-08 10:44:50 +0545

    Chore: Initial commit

---
+++ image/__init__.py
@@ -0,0 +1,26 @@
+from PIL import Image, ImageDraw, ImageFont
+
+base_image = Image.new(
+    "RGBA", (330, 50)
+)
+
+def generate_image(asset_image, color, status, *, font="./assets/font.ttf"):
+    
+    base_copy = base_image.copy()
+    
+    drawable_font = ImageFont.truetype(font, size=32)
+    content_image = Image.open(asset_image).resize(
+        (50, 50)
+    )
+
+    base_copy.paste(
+        content_image, (15, 0)
+    )
+
+    drawable = ImageDraw.Draw(base_copy)
+
+    drawable.text(
+        (75, 5), status, color, drawable_font, align="center"
+    )
+
+    return base_copy
