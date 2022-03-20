2c31a2c17360039d6e2082c52fb599b1cbe20e27
justfoolingaround <kr.justfoolingaround@gmail.com>
2022-03-20 13:20:29 +0545

    Chore: `Cookie` fix

--- generator.py
+++ generator.py
@@ -8,37 +8,40 @@ from image import generate_image
 
 
 def animepahe_one_piece(*, query="One Piece"):
-    return "https://animepahe.com/anime/{[session]}".format(client.get(
-        'https://animepahe.com/api?m=search&l=8&q={}'.format(query)
-    ).json().get('data')[0])
+    return "https://animepahe.com/anime/{[session]}".format(
+        client.get("https://animepahe.com/api?m=search&l=8&q={}".format(query))
+        .json()
+        .get("data")[0]
+    )
 
 
 FAILED = ("./assets/failed.png", (218, 68, 83))
 SUCCESS = ("./assets/success.png", (50, 198, 113))
 
 site_check_index = {
-    'nineanime': 'https://9anime.to/watch/one-piece.ov8',
-    'animepahe': animepahe_one_piece(),
-    'crunchyroll': 'https://www.crunchyroll.com/one-piece',
-    'allanime': 'https://allanime.site/anime/ReooPAxPMsHM4KPMY',
-    'animeout': 'https://www.animeout.xyz/download-one-piece-episodes-latest/',
-    'animixplay': 'https://animixplay.to/v1/one-piece',
-    'animtime': 'https://animtime.com/title/5',
-    'gogoanime': 'https://gogoanime.cm/category/one-piece',
-    'tenshi': 'https://tenshi.moe/anime/kjfrhu3s',
-    'twist': 'https://twist.moe/a/one-piece',
-    'kawaiifu': 'https://kawaiifu.com/tv-series/one-piece-720p-hd.html',
-    'haho': 'https://haho.moe/anime/sjmjiywi',
-    'zoro': 'https://zoro.to/one-piece-100',
+    "nineanime": "https://9anime.to/watch/one-piece.ov8",
+    "animepahe": animepahe_one_piece(),
+    "crunchyroll": "https://www.crunchyroll.com/one-piece",
+    "allanime": "https://allanime.site/anime/ReooPAxPMsHM4KPMY",
+    "animeout": "https://www.animeout.xyz/download-one-piece-episodes-latest/",
+    "animixplay": "https://animixplay.to/v1/one-piece",
+    "animtime": "https://animtime.com/title/5",
+    "gogoanime": "https://gogoanime.cm/category/one-piece",
+    "tenshi": "https://tenshi.moe/anime/kjfrhu3s",
+    "twist": "https://twist.moe/a/one-piece",
+    "kawaiifu": "https://kawaiifu.com/tv-series/one-piece-720p-hd.html",
+    "haho": "https://haho.moe/anime/sjmjiywi",
+    "zoro": "https://zoro.to/one-piece-100",
 }
 
+
 def site_check(url):
-    
+
     initial = time.perf_counter()
-    
+
     try:
-        links = (get_appropriate(client, url, lambda r: r == 1))
-        
+        links = get_appropriate(client, url, lambda r: r == 1)
+
         if not links:
             return generate_image(*FAILED, "No return")
 
@@ -49,29 +52,25 @@ def site_check(url):
                 obtained_links += 1
 
         if not obtained_links:
-            return generate_image(
-                *FAILED, "No links returned"
-            )
-
+            return generate_image(*FAILED, "No links returned")
 
         return generate_image(
-            *SUCCESS, "{} url(s), {:.02f}s".format(obtained_links, time.perf_counter() - initial)
+            *SUCCESS,
+            "{} url(s), {:.02f}s".format(obtained_links, time.perf_counter() - initial)
         )
-
 
     except Exception as _:
-        return generate_image(
-            *FAILED, "Project exception"
-        )
+        return generate_image(*FAILED, "Project exception")
 
-with open('./api/raw', 'w') as raw_file:
-    raw_file.write(client.get('http://crunchyroll.com/').cookies.get('session_id'))
+
+with open("./api/raw", "w") as raw_file:
+    raw_file.write(
+        client.get("http://crunchyroll.com/").cookies.get("session_id", "no cookie")
+    )
 
 
 for sitename, site in site_check_index.items():
-    
+
     img = site_check(site)
-    img.save(
-        "./api/providers/{}.png".format(sitename), format="png"
-    )
+    img.save("./api/providers/{}.png".format(sitename), format="png")
     img.close()
--- image/__init__.py
+++ image/__init__.py
@@ -1,26 +1,19 @@
 from PIL import Image, ImageDraw, ImageFont
 
-base_image = Image.new(
-    "RGBA", (420, 50)
-)
+base_image = Image.new("RGBA", (420, 50))
+
 
 def generate_image(asset_image, color, status, *, font="./assets/font.ttf"):
-    
-    base_copy = base_image.copy()
-    
-    drawable_font = ImageFont.truetype(font, size=32)
-    content_image = Image.open(asset_image).resize(
-        (50, 50)
-    )
 
-    base_copy.paste(
-        content_image, (15, 0)
-    )
+    base_copy = base_image.copy()
+
+    drawable_font = ImageFont.truetype(font, size=32)
+    content_image = Image.open(asset_image).resize((50, 50))
+
+    base_copy.paste(content_image, (15, 0))
 
     drawable = ImageDraw.Draw(base_copy)
 
-    drawable.text(
-        (80, 5), status, color, drawable_font, align="center"
-    )
+    drawable.text((80, 5), status, color, drawable_font, align="center")
 
     return base_copy
